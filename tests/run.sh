b703eb2134505cc608ffc610af818d5d92f55e0e
Marcin Sztolcman <m.sztolcman@36monkeys.com>
2016-05-21 11:32:07 +0200

    tests: show tested php version in tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -2,8 +2,13 @@
 
 set -o nounset
 
+echo 5.3
 docker run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.3-cli vendor/bin/phpunit "$@"
+echo 5.4
 docker run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.4-cli vendor/bin/phpunit "$@"
+echo 5.5
 docker run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.5-cli vendor/bin/phpunit "$@"
+echo 5.6
 docker run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.6-cli vendor/bin/phpunit "$@"
+echo 7.0
 docker run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.0-cli vendor/bin/phpunit "$@"

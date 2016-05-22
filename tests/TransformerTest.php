a5fcf073e20ee452e8b08db89d715de1a16dc33e
Marcin Sztolcman <m.sztolcman@36monkeys.com>
2016-05-22 11:38:26 +0200

    fixed test for upperWords

--- tests/TransformerTest.php
+++ tests/TransformerTest.php
@@ -226,7 +226,7 @@ class TransformerTest extends \PHPUnit_Framework_TestCase
     {
         $src = 'abc abd aBE';
         $tr = new Transformer($src);
-        $res = $tr->upperWords('UTF-8');
+        $res = $tr->upperWords();
         $this->assertEquals('Abc Abd ABE', (string) $res);
     }
 
@@ -239,7 +239,7 @@ class TransformerTest extends \PHPUnit_Framework_TestCase
         $this->markTestSkipped('not finished yet');
         $src = 'ąbć ćbd ęBĘ';
         $tr = new Transformer($src);
-        $res = $tr->upperWords();
+        $res = $tr->upperWords('UTF-8');
         $this->assertEquals('Ąbć Ćbd ĘBĘ', (string) $res);
     }
 

26c376e8e6ea4e26b560c4f243229cf59b2d92b3
Marcin Sztolcman <m.sztolcman@36monkeys.com>
2017-03-26 19:29:43 +0200

    changed 36monkeys url to https protocol

--- src/Functional.php
+++ src/Functional.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of m36/stringformatter.
  *
- * (c) 36monkeys <http://36monkeys.com>
+ * (c) 36monkeys <https://36monkeys.com>
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
